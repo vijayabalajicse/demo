008c789053b534fc63e6020bc729e032af31d8d9
vijayabalajicse <vijaya.balaji@a-cti.com>
2014-01-06 22:09:19 +0530

    Signed-off-by: vijayabalajicse <vijaya.balaji@a-cti.com>

---
+++ src/dem2/Demo1Servlet.java
@@ -0,0 +1,13 @@
+package dem2;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Demo1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
